d52d81d1e3bc531c60c15243e638fe7d0ee8e7c6
MatthewBrummund <143558807+MatthewBrummund@users.noreply.github.com>
2024-09-09 16:14:08 -0700

    Project creation

---
+++ nj_cdk_app/config.py
@@ -0,0 +1,23 @@
+
+
+def vpc_id():
+    input_vpc_id = input("Enter the VPC ID: ")
+    return input_vpc_id
+
+def vpc_subnet_id():
+    input_vpc_subnet_id = input("Enter the VPC Subnet ID: ")
+    return input_vpc_subnet_id
+
+def region():
+    input_region = input("Enter the region: ")
+    return input_region
+
+def account():
+    input_account = input("Enter the account number: ")
+    return input_account
+
+
+vpc_id_test = "vpc-08216c86201f5a3c7"
+vpc_subnet_id_test = "subnet-07703c62ab316c87c"
+region_test = "us-east-1"
+account_test = "314146302325"
